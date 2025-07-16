614491d6d32eefab6ef65eb559dc2a2688b71eca
Devasaya2003 <devasyasingh2000@gmail.com>
2025-07-16 17:56:48 +0530

    base lsp setup done

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,95 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "clangd",
+          "dockerls",
+          "eslint",
+          "html",
+          "jsonls",
+          "prismals",
+          "ts_ls",
+          "cssls",
+          "tailwindcss",
+          "sqlls",
+          "pyright",
+          "marksman",
+          "lemminx",
+          "yamlls"
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require('lspconfig')
+
+      -- Function to setup LSP
+      local on_attach = function(client, bufnr)
+        -- Keymaps for LSP actions
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { buffer = bufnr })
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+      end
+
+      -- Setup LSP servers
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.clangd.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.dockerls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.eslint.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.html.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.jsonls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.prismals.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.ts_ls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.cssls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.tailwindcss.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.sqlls.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.marksman.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.lemminx.setup({
+        on_attach = on_attach,
+      })
+      lspconfig.yamlls.setup({
+        on_attach = on_attach,
+      })
+    end
+  }
+}
+
