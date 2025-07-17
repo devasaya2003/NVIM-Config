d6e79709efef581213653430686391540676091f
Devasaya2003 <devasyasingh2000@gmail.com>
2025-07-17 07:31:48 +0530

    added 3 terminal support

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -2,18 +2,30 @@ return {
   "akinsho/toggleterm.nvim",
   config = function()
     require("toggleterm").setup({
-      direction = "float",
-      persist_mode = true,
+      size = 15,
+      direction = "horizontal",
       close_on_exit = false,
+      persist_mode = true,
       auto_scroll = true,
       float_opts = {
         border = "rounded",
-        width = 80,
-        height = 20,
+        width = function() return math.floor(vim.o.columns * 0.8) end,
+        height = function() return math.floor(vim.o.lines * 0.8) end,
       },
     })
 
-    vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<CR>") -- Toggle terminal
-    vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])             -- Exit terminal mode
-  end,
+    local Terminal = require("toggleterm.terminal").Terminal
+    local project_root = vim.fn.getcwd()
+
+    local terminals = {
+      main = Terminal:new({ direction = "horizontal", dir = project_root }),
+      git = Terminal:new({ direction = "horizontal", hidden = true, dir = project_root }),
+      runner = Terminal:new({ direction = "horizontal", hidden = true, dir = project_root })
+    }
+
+    vim.keymap.set("n", "<leader>tt", function() terminals.main:toggle() end) -- Toggle main terminal
+    vim.keymap.set("n", "<leader>tg", function() terminals.git:toggle() end) -- Toggle git terminal
+    vim.keymap.set("n", "<leader>tr", function() terminals.runner:toggle() end) -- Toggle runner terminal
+    vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]]) -- Exit terminal mode
+  end
 }
